b50908dabac5c2d75e5489a9e0e4e6290f23790d
Tomáš Horáček <tomas.horacek@gmail.com>
2023-06-20 18:09:23 +0200

    demo 1 - step 3

---
+++ apps/quacker-frontend/src/app/App.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './App.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="quacker-frontend" />
+    </div>
+  );
+}
+
+export default App;
