f6a6d076727d468af9eb7efa87d7b5b146253e71
Tomáš Horáček <tomas.horacek@gmail.com>
2023-06-20 18:13:29 +0200

    demo 1 - step 4

--- apps/quacker-backend/src/main.ts
+++ apps/quacker-backend/src/main.ts
@@ -1,21 +1,103 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import dotenv from 'dotenv-flow';
 import express from 'express';
-import * as path from 'path';
+import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
-const app = express();
+import { getConnection } from './libs/connection';
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+import rootResolver from './modules/rootResolver';
+import mockResolver from './__mocks__/mockResolver';
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to quacker-backend!' });
-});
+dotenv.config();
 
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+const MOCKS = process.env.MOCKS === 'true';
+
+const typeDefs = gql`
+  type User {
+    id: Int!
+    name: String!
+    userName: String!
+    profileImageUrl: String
+    quacks: [Quack!]!
+  }
+
+  type Quack {
+    id: Int!
+    createdAt: String!
+    user: User!
+    userId: Int!
+    text: String!
+  }
+
+  type AuthUser {
+    id: Int!
+    name: String!
+    userName: String!
+    profileImageUrl: String
+  }
+
+  type AuthInfo {
+    user: AuthUser!
+    token: String!
+  }
+
+  type Query {
+    users: [User!]!
+    user(userName: String!): User
+    quacks: [Quack!]!
+  }
+
+  type Mutation {
+    signin(email: String!, password: String!): AuthInfo!
+
+    signup(
+      email: String!
+      password: String!
+      name: String!
+      userName: String!
+      profileImageUrl: String
+    ): AuthInfo!
+
+    addQuack(userId: Int!, text: String!): Quack!
+  }
+`;
+
+const main = async () => {
+  const app = express();
+
+  app.disable('x-powered-by');
+  app.use(cors());
+
+  const dbConnection = MOCKS ? null : await getConnection();
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers: MOCKS ? mockResolver : rootResolver,
+    context: async ({ req, res }) => {
+      const auth = req.headers.Authorization || '';
+
+      return {
+        req,
+        res,
+        dbConnection,
+        auth,
+      };
+    },
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  const port = process.env.PORT || 4000;
+
+  app.get('/', (_, res) => res.redirect('/graphql'));
+
+  app.listen(port, () => {
+    console.info(`Server started at http://localhost:${port}/graphql`);
+  });
+};
+
+main();
